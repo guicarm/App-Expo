e77b78875373c77799b416648b540753a95ebd99
guicarm <RM98874@fiap.com.br>
2024-09-30 10:40:36 -0300

    add: authContext Provider

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -8,6 +8,35 @@ export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
 
     useEffect( () => {
+        const loadUserData = async() => {
+            const storedUser = await AsyncStorage.getItem("user")
+            if(storedUser) setUser(JSON.parse(storedUser))
+        }
+        loadUserData()
+    }, [])
 
-    }, [] )
+    // LOGIN
+    const login = async (email, password) => {
+        if(email === "teste@teste.com" && password === "password1234") {
+            const userData = {id: 1, email}
+            setUser(userData)
+            await AsyncStorage.setItem("user", JSON.stringify(userData)); 
+        } else {
+            throw new Error("Invalid email and/or password")
+        }
+    }   
+
+    
+    // LOGOUT
+    const logout = async() => {
+        setUser(null)
+        await AsyncStorage.removeItem("user");
+    }
+
+
+    return (
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
